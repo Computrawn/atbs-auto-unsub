7041f16d285673a30b80b4052301f8264d41a7b9
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-12 20:14:20 -0500

    Initial commit.

---
+++ auto_unsubscriber.py
@@ -0,0 +1,12 @@
+#! python3
+# filename.py — Short description of project goes here.
+# For more information, see project_details.txt.
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.

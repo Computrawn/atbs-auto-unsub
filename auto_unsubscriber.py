904d0e02b6fd7ea2b1c3c990b18688d1fd5068c7
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-15 15:59:36 -0500

    Finish functionality.

--- auto_unsubscriber.py
+++ auto_unsubscriber.py
@@ -5,6 +5,8 @@
 from datetime import date
 import logging
 import os
+import webbrowser
+from bs4 import BeautifulSoup
 from imapclient import IMAPClient
 import pyzmail
 
@@ -13,21 +15,29 @@ logging.basicConfig(
     filename="logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-logging.disable(logging.DEBUG)  # Note out to enable logging.
+# logging.disable(logging.DEBUG)  # Note out to enable logging.
 
 IMAP_HOST = os.environ.get("ICLD_IMAP")
 IMAP_ADDRESS = os.environ.get("ICLD_USER")
 IMAP_PASSWORD = os.environ.get("ICLD_PASS")
 
+SEARCH_BEGIN_DATE = date(2023, 5, 1)
+
+unsubscribe_urls = []
+
 with IMAPClient(IMAP_HOST) as client:
     client.login(IMAP_ADDRESS, IMAP_PASSWORD)
     client.select_folder("INBOX", readonly=True)
-    UIDs = client.search(["SINCE", date(2023, 5, 1)])
+    UIDs = client.search(["SINCE", SEARCH_BEGIN_DATE])
 
     for uid in UIDs:
         raw_message = client.fetch([uid], ["BODY[]", "FLAGS"])
         message = pyzmail.PyzMessage.factory(raw_message[uid][b"BODY[]"])
         if message.html_part is not None:
-            logging.info(
-                message.html_part.get_payload().decode(message.html_part.charset)
-            )
+            html_msg = message.html_part.get_payload().decode(message.html_part.charset)
+            soup = BeautifulSoup(html_msg, features="html.parser")
+            for link in soup.find_all("a", string=["Unsubscribe", "unsubscribe"]):
+                unsubscribe_urls.append(link.get("href"))
+
+for url in unsubscribe_urls:
+    webbrowser.open(url)
